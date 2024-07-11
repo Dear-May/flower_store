c117cf30265cb047e354bf73293311c4d669f4ec
月月睡不醒 <121281524+Dear-May@users.noreply.github.com>
2024-07-11 17:32:20 +0800

    2024年7月11日17:31:44 整理项目文件 增加功能清单

--- src/main/java/com/example/demo/service/UserIService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.Entity.UserTestEntity;
-import java.util.List;
-
-public interface UserIService {
-
-    public List<UserTestEntity> queryAllUser();
-}
