45c509ceee39300ad76b9f2d9baa7f3d8fa9911f
wszzs <151749366+wszzs@users.noreply.github.com>
2024-07-12 17:24:28 +0800

    修改数据库字段

--- src/main/java/com/example/demo/Mapper/GoodsMapper.java
+++ src/main/java/com/example/demo/Mapper/GoodsMapper.java
@@ -6,18 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public interface GoodsMapper {
 
-    @Update("update goods set GoodsState='已下架' where GoodsID=#{GoodsID}")
+    @Update("update goods set GoodsState='已下架' where id=#{GoodsID}")
     public boolean DeleteGoods(@Param("GoodsID")int GoodsID);
 
-    @Update("update goods set GoodsName=#{GoodsName} where GoodsID=#{GoodsID}")
+    @Update("update goods set goods_name=#{GoodsName} where id=#{GoodsID}")
     public boolean UpdateGoodName(@Param("GoodsID")int GoodsID,@Param("GoodsName")String GoodsName);
 
-    @Update("update goods set GoodsDescribe=#{GoodsDescribe} where GoodsID=#{GoodsID}")
+    @Update("update goods set goods_describe=#{GoodsDescribe} where id=#{GoodsID}")
     public boolean UpdateGoodDes(@Param("GoodsID")int GoodsID,@Param("GoodsDescribe")String GoodsDescribe);
 
-    @Update("update goods set GoodPrice=#{GoodPrice} where GoodsID=#{GoodsID}")
+    @Update("update goods set good_price=#{GoodPrice} where id=#{GoodsID}")
     public boolean UpdateGoodPrice(@Param("GoodsID")int GoodsID,@Param("GoodPrice")String GoodPrice);
 
-    @Update("update goods set GoodsCategory=#{GoodsCategory} where GoodsID=#{GoodsID}")
+    @Update("update goods set goods_category=#{GoodsCategory} where id=#{GoodsID}")
     public boolean UpdateGoodsCategory(@Param("GoodsID")int GoodsID,@Param("GoodsCategory")String GoodsCategory);
 }
