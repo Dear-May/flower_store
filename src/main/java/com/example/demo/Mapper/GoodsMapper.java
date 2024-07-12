4a49a3f37b300ff43453f932b1315f329cb3165f
wszzs <151749366+wszzs@users.noreply.github.com>
2024-07-12 22:58:32 +0800

    完成修改商品信息 添加商品 查看全部商品 按种类查看商品 下架商品 商品加入购物车

--- src/main/java/com/example/demo/Mapper/GoodsMapper.java
+++ src/main/java/com/example/demo/Mapper/GoodsMapper.java
@@ -1,12 +1,15 @@
 package com.example.demo.Mapper;
+import com.example.demo.Entity.GoodEntity;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Mapper
 @Component
 public interface GoodsMapper {
 
-    @Update("update goods set GoodsState='已下架' where id=#{GoodsID}")
+    @Update("update goods set goods_state='已下架' where id=#{GoodsID}")
     public boolean DeleteGoods(@Param("GoodsID")int GoodsID);
 
     @Update("update goods set goods_name=#{GoodsName} where id=#{GoodsID}")
@@ -20,4 +23,26 @@ public interface GoodsMapper {
 
     @Update("update goods set goods_category=#{GoodsCategory} where id=#{GoodsID}")
     public boolean UpdateGoodsCategory(@Param("GoodsID")int GoodsID,@Param("GoodsCategory")String GoodsCategory);
+
+    @Insert("insert into goods(goods_name, goods_describe,good_price,goods_category,goods_imgurl) values(#{goods_name}, #{goods_describe}, #{good_price}, #{goods_category}, #{goods_imgurl})")
+    public boolean addGoods(@Param("goods_name") String goods_name, @Param("goods_describe") String goods_describe, @Param("good_price") String good_price, @Param("goods_category") String goods_category, @Param("goods_imgurl") String goods_imgurl);
+
+    @Select("select * from goods")
+    public List<GoodEntity> selectGood();
+
+    @Select("select * from goods where goods_category=#{goods_category}")
+    public List<GoodEntity> selectGoodByCate(@Param("goods_category") String goods_category);
+
+    @Select("SELECT * from goods where id=#{id}")
+    public GoodEntity selectGoodInoById(@Param("id")int id);
+
+    @Select("SELECT count(*) from shopping_cart where user_id=#{userId} and cart_goodid=#{GoodsID}")
+    public int selectCartRecord(@Param("userId") int userId,@Param("GoodsID")int GoodsID);//查询当前用户是否已经将这个商品加入购物车
+
+    @Insert("insert into shopping_cart (user_id,cart_goodid) values(#{GoodsID},#{userId})")
+    public boolean insertShopCart(@Param("GoodsID")int GoodsID,@Param("userId") int userId);//给购物车新建一条记录
+
+    @Update("update shopping_cart set cart_goodnum=cart_goodnum +1 where user_id=#{userId} and cart_goodid=#{GoodsID}")
+    public boolean CartGoodIncrement(@Param("GoodsID")int GoodsID,@Param("userId") int userId);
+
 }
