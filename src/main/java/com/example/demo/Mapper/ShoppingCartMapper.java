51a26bc07ba2932f057894c8bc74a98278603319
wszzs <151749366+wszzs@users.noreply.github.com>
2024-07-13 00:09:46 +0800

    购物车sql

--- src/main/java/com/example/demo/Mapper/ShoppingCartMapper.java
+++ src/main/java/com/example/demo/Mapper/ShoppingCartMapper.java
@@ -2,9 +2,7 @@ package com.example.demo.Mapper;
 
 import com.example.demo.Entity.GoodEntity;
 import com.example.demo.Entity.GoodEntityGet;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,10 +11,18 @@ import java.util.Map;
 @Mapper
 @Component
 public interface ShoppingCartMapper {
-    @Select("select goods.* from shopping_cart inner join goods on shopping_cart.cart_goodid=goods.id where user_id=#{userId} and cart_goodid=#{GoodsID}")
-    public List<GoodEntityGet> findPersonalGoodInCart(@Param("userId") int userId,@Param("GoodsID")int GoodsID);
+    @Select("select goods.* from shopping_cart inner join goods on shopping_cart.cart_goodid=goods.id where user_id=#{userId} ")
+    public List<GoodEntityGet> findPersonalGoodInCart(@Param("userId") int userId);
 
     @Select("SELECT cart_goodnum from shopping_cart where user_id=#{userId} and cart_goodid=#{GoodsID}")
-    public int getgoodnumber(@Param("userId") int userId,@Param("GoodsID")int GoodsID);//查询当前用户是否已经将这个商品加入购物车
+    public int getgoodnumber(@Param("userId") int userId,@Param("GoodsID")int GoodsID);//查询这个商品的数量
 
+    @Update("update shopping_cart set cart_goodnum=cart_goodnum +1 where user_id=#{userId} and cart_goodid=#{GoodsID}")
+    public boolean CartGoodIncrement(@Param("userId") int userId,@Param("GoodsID")int GoodsID);//增加数量
+
+    @Update("update shopping_cart set cart_goodnum=cart_goodnum -1 where user_id=#{userId} and cart_goodid=#{GoodsID}")
+    public boolean CartGoodDecrement(@Param("userId") int userId,@Param("GoodsID")int GoodsID);//减少数量
+
+    @Delete("delete from shopping_cart where user_id=#{userId} and cart_goodid=#{GoodsID}")
+    public boolean CartDelete(@Param("userId") int userId,@Param("GoodsID")int GoodsID);
 }
