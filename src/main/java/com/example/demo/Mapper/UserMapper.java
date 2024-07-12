9c5e695619f768902fdbd1472f0da72a8d318538
wszzs <151749366+wszzs@users.noreply.github.com>
2024-07-12 17:24:34 +0800

    修改数据库字段

--- src/main/java/com/example/demo/Mapper/UserMapper.java
+++ src/main/java/com/example/demo/Mapper/UserMapper.java
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Component;
 @Component
 public interface UserMapper {
 
-    @Select("select userName from user where userName = #{userName}")
+    @Select("select user_name from user where user_name = #{userName}")
     public String selectUserName(String userName);
 
-    @Select("select userPassword from user where userName = #{userName}")
+    @Select("select user_password from user where user_name = #{userName}")
     public String selectUserPassword(String userName);
 
-    @Insert("insert into user(userName, userPassword) values(#{userName}, #{userPassword})")
+    @Insert("insert into user(user_name, user_password) values(#{userName}, #{userPassword})")
     public void addUser(@Param("userName") String userName, @Param("userPassword") String userPassword);
 
-    @Update("update goods set GoodsState='已下架' where GoodsID=#{GoodsID}")
-    public boolean DeleteGoods(@Param("GoodsID")int GoodsID);
-
-    @Update("update goods set GoodsName=#{GoodsName} where GoodsID=#{GoodsID}")
-    public boolean UpdateGoodName(@Param("GoodsID")int GoodsID,@Param("GoodsName")String GoodsName);
+//    @Update("update goods set GoodsState='已下架' where GoodsID=#{GoodsID}")
+//    public boolean DeleteGoods(@Param("GoodsID")int GoodsID);
+//
+//    @Update("update goods set GoodsName=#{GoodsName} where GoodsID=#{GoodsID}")
+//    public boolean UpdateGoodName(@Param("GoodsID")int GoodsID,@Param("GoodsName")String GoodsName);
 }
 
